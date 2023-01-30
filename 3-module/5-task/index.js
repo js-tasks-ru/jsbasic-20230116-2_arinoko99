619ab9eab702c557769225219d50fb2eed9629df
Arina Gerdasova <arinoko99@gmail.com>
2023-01-30 13:02:11 +0700

    Решение задач 3 урока

--- 3-module/5-task/index.js
+++ 3-module/5-task/index.js
@@ -1,3 +1,19 @@
 function getMinMax(str) {
   // ваш код...
+
+  let array = str.split(" ");
+
+  let minValue = +array[0];
+  let maxValue = minValue;
+  for (let i = 0; i < array.length; i++) {
+    let el = +array[i];
+    if (el < minValue) {
+      minValue = el;
+    }
+    if (el > maxValue) {
+      maxValue = el;
+    }
+  }
+  return {min: minValue, max: maxValue};
+
 }
