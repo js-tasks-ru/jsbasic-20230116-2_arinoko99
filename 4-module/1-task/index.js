60af518b35fc4cf71adeb180ced4c35aaab7dfad
Arina Gerdasova <arinoko99@gmail.com>
2023-02-02 18:08:26 +0700

    Решение задач четвертого урока

--- 4-module/1-task/index.js
+++ 4-module/1-task/index.js
@@ -1,3 +1,13 @@
 function makeFriendsList(friends) {
   // ваш код...
+
+  let list = document.createElement('ul');
+
+  friends.forEach(el => {
+    let listEl = document.createElement('li');
+    listEl.innerHTML = el.firstName + " " + el.lastName;
+    list.append(listEl);
+  });
+
+  return list;
 }
